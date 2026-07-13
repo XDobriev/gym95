2ec6741c17e8f32ef85df8164812adfdc6f8740b
Khamza Dobriev <XDobriev@yandex.ru>
2026-07-13 23:35:56 +0300

    refactor: extract shared Telegraf instance into botInstance.ts

---
+++ src/botInstance.ts
@@ -0,0 +1,19 @@
+import { Telegraf } from 'telegraf';
+import { config } from './config';
+import { registerStart } from './handlers/start';
+import { registerHistory } from './handlers/history';
+import { registerProgress } from './handlers/progress';
+import { registerExport } from './handlers/export';
+import { registerWorkout } from './handlers/workout';
+
+export const bot = new Telegraf(config.BOT_TOKEN);
+
+registerStart(bot);
+registerWorkout(bot);
+registerHistory(bot);
+registerProgress(bot);
+registerExport(bot);
+
+bot.catch((err, ctx) => {
+  console.error(`Ошибка обработки апдейта ${ctx.updateType}:`, err);
+});
